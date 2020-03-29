15d8efd22659e5ff28de7cdae3703df14a15e091
japurcell <japurcell@users.noreply.github.com>
2020-03-29 13:14:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# WordPress
+
+## Documentation
+  * [Docker Wordpress for Development](https://davidyeiser.com/tutorials/docker-wordpress-theme-setup)
+  * [Docker Wordpress](https://docs.docker.com/compose/wordpress/)
+  * [Docker Compose Up](https://docs.docker.com/compose/reference/up/)
+  * [Official WP Dev Docs](https://developer.wordpress.org/)
+  * [PHP Language](https://www.php.net/manual/en/langref.php)
+
+## WordPress Examples
+  * [Angular with APIs](https://www.kevinleary.net/angularjs-wordpress-tutorial/)
+  * [PHP Namespaces](https://github.com/iandunn/compassionate-comments)
+
+## Best Practices
+  * [Plugin Dev: Know these 10 things](https://www.smashingmagazine.com/2011/03/ten-things-every-wordpress-plugin-developer-should-know/)
---
+++ wp-content/plugins/my-api/my-api.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: My API
+description:
+Version: 1.0
+Text Domain: my-api
+*/
+
+defined('ABSPATH') or die('No script kiddies please!');
+
+
+function myapi_hello_func($atts)
+{
+  $issues = [
+    ['title' => 'Issue 1'],
+    ['title' => 'Issue 2']
+  ];
+
+  // Handle the case when there are no issues
+  if (empty($issues))
+    return "<strong>" . __("No issues to show", 'githup-api') . "</strong>";
+
+  // We're going to return a string. First, we open a list.
+  $return = "<ul>";
+
+  // Loop over the returned issues
+  foreach ($issues as $issue) {
+
+    // Add a list item for each issue to the string
+    // (Feel free to get fancier here)
+    // Maybe make each one a link to the issue issuing $issue['url] )
+    $return .= "<li>{$issue['title']}</li>";
+  }
+
+  // Don't forget to close the list
+  $return .= "</ul>";
+
+  return $return;
+}
+
+add_shortcode("myapi_hello", "myapi_hello_func");
